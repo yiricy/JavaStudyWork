0caa5b6fe5b2c21a48db324d977dd215f289fce3
yiricy <43192662+yiricy@users.noreply.github.com>
2020-11-05 15:16:28 +0800

    Add files via upload

--- UUIDUtil.java
+++ UUIDUtil.java
@@ -1,4 +1,4 @@
-
+package com.javaoop.xsb;
 import java.util.UUID;
 /**
  * UUID工具类
@@ -6,24 +6,21 @@ import java.util.UUID;
  * @author yiricy
  * @date 2020年11月5日
  */
-public class UUIDUtil {
+public final class UUIDUtil {
 	
 	//传入需要获取的随机序列长度，返回一个随机序列
 	public static String randomString(int lenth) {
-		String replace1 = UUID.randomUUID().toString().replace("-", "");
 		
+		//创建StringBuilder对象
+		StringBuilder sb = new StringBuilder();
 		
 		//长度不够时，扩容
-		
-		while (replace1.length() < lenth) {
-
-			String replace2 = UUID.randomUUID().toString().replace("-", "");
-			replace1 += replace2;
-				
+		 do {
+			 sb.append(UUID.randomUUID().toString().replace("-", ""));	
+		} while (sb.length() < lenth);
+			
+		return sb.substring(0, lenth);
 		}
-		return replace1.substring(0, lenth);
-	
-	}
 	
 	//传入需要获取的随机序列长度，和需要的序列类型：传入letter返回纯字母序列，传入number返回纯数字序列
 	public static String randomString(int lenth , String needString) {
@@ -32,40 +29,35 @@ public class UUIDUtil {
 		//接收参数为letter时
 		if(needString.equals("letter")) {
 			
-			//生成一个只有字母的字符串
-			String UUIDStr = UUID.randomUUID().toString().replace("-", "").replaceAll("\\d+","");
+			//创建StringBuilder对象
+			StringBuilder sb = new StringBuilder();
 			
-			while (UUIDStr.length() < lenth) {
-				
-				//长度不够时，扩容
-				String replace2 = UUID.randomUUID().toString().replace("-", "").replaceAll("\\d+","");
-				UUIDStr += replace2;
-			}
+			//长度不够时，扩容
+			 do {
+				 sb.append(UUID.randomUUID().toString().replace("-", "").replaceAll("\\d+",""));	
+			} while (sb.length() < lenth);
 			
-			
-			return UUIDStr.substring(0, lenth);
+			return sb.substring(0, lenth);
 		}
 		
 		
 		//接受参数为number时
-		if (needString.equals("number")) {
-			//生成一个只有字母的字符串
-			String UUIDStr = UUID.randomUUID().toString().replace("-", "").replaceAll("[a-zA-Z]","");
+		if(needString.equals("letter")) {
 			
-			while (UUIDStr.length() < lenth) {
-				
-				//长度不够时，扩容
-				String replace2 = UUID.randomUUID().toString().replace("-", "").replaceAll("[a-zA-Z]","");
-				UUIDStr += replace2;
-			}
+			//创建StringBuilder对象
+			StringBuilder sb = new StringBuilder();
 			
+			//长度不够时，扩容
+			 do {
+				 sb.append(UUID.randomUUID().toString().replace("-", "").replaceAll("[a-zA-Z]",""));	
+			} while (sb.length() < lenth);
 			
-			return UUIDStr.substring(0, lenth);
+			return sb.substring(0, lenth);
 		}
 		
 		
 		return "参数异常";
-	
+		
 		
 	}
 }
